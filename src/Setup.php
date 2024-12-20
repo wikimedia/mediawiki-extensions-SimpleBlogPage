bbfefc6a2ac147b432edab4d481502cbcca632ee
d.savuljesku <d.savuljesku@gmail.com>
2024-12-20 14:25:05 +0100

    Enable comments

--- src/Setup.php
+++ src/Setup.php
@@ -8,5 +8,10 @@ class Setup {
 
 		$GLOBALS['wgVisualEditorAvailableContentModels']['blog_post'] = 'article';
 		$GLOBALS['wgVisualEditorAvailableContentModels']['blog_root'] = 'article';
+
+		$GLOBALS['wgExtensionFunctions'][] = static function() {
+			$GLOBALS['wgCommentStreamsAllowedNamespaces'][] = NS_BLOG;
+			$GLOBALS['wgCommentStreamsAllowedNamespaces'][] = NS_USER_BLOG;
+		};
 	}
 }
